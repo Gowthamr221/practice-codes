22e17fe9b4a40faca8f98340f1b1fb41a21f0ce9
Gowthamr221 <gowthamr221@gmail.com>
2022-03-08 23:46:13 +0530

    march 8th

---
+++ timeComplexity/insertionsort.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+
+// insertion sort
+	vector<int>arr = {8,4,1,5,9,2};
+
+
+
+}
