9ed7505f987d355d1e2d537ad606010c7b8c9ef0
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-03-30 23:40:48 +0530

    Time: 4 ms (77.73%), Space: 9.6 MB (5.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,62 +2,21 @@ class Solution {
 
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.size()==0){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        if(m==0 || n==0){
             return false;
         }
-        // if(matrix.size()==1&&matrix[0].size()==1){
-        //     if(target==matrix[0][0]){
-        //         return true;
-        //     }
-        //     else{
-        //         return false;
-        //     }
-        // }
+        int low =0,high = m*n-1;
         
-        int lessThan = 0;
-        int low =0;
-        int high = matrix.size()-1;
-
         while(low<=high){
-            int mid = low + (high-low)/2;
-            if(matrix[mid][0]==target){
+            int mid = low+(high-low)/2;
+            int val = matrix[mid/n][mid%n];
+            if(val==target){
                 return true;
             }
-            if(matrix[mid][0]>=target){
-                if(mid>0)
-                lessThan = mid-1;
-            }
-            if(matrix[mid][0] < target){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        int searchIndex = lessThan;
-        low = 0;
-        high = matrix[0].size()-1;
-        while(low<=high){
-            int mid = low + (high - low)/2;
-            if(matrix[searchIndex][mid]==target){
-                return true;
-            }
-            if(matrix[searchIndex][mid]<target){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-           low = 0;
-        high = matrix[0].size()-1;
-        while(low<=high){
-            int mid = low + (high - low)/2;
-            if(matrix[matrix.size()-1][mid]==target){
-                return true;
-            }
-            if(matrix[matrix.size()-1][mid]<target){
-                low = mid+1;
+            if(val<target){
+                low=mid+1;
             }
             else{
                 high = mid-1;
