493426118cd4194eb3414d8042c10a1afe522e74
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-01 23:27:45 +0530

    Time: 764 ms (31.47%), Space: 8.1 MB (36.15%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,32 +1,38 @@
 
 class Solution {
 private:
-    int m,n;
-    bool isFound(vector<vector<char>>&board,const char *w,int x,int y){
-        if(x<0||y<0||x>=m||y>=n||board[x][y]=='\0'||*w!=board[x][y])
-    			return false;
-        if(*(w+1)=='\0'){
+    int row,col;
+    int dx[4]={0,1,0,-1};
+    int dy[4]={1,0,-1,0};
+    
+    bool search(int i,int j,vector<vector<char>>&board,string word,unsigned int currIndex){
+        if(currIndex>=word.size()){
             return true;
         }
-        char t = board[x][y];
-        board[x][y]='\0';
-            		if(isFound(board,w+1,x-1,y)||isFound(board,w+1,x+1,y)||isFound(board,w+1,x,y-1)||isFound(board,w+1,x,y+1)) return true;
-        board[x][y]=t;
+        if(i<0 || j<0 || i>=row || j>=col || board[i][j]!=word[currIndex]){
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j]='#';
+        for(int k=0;k<4;k++){
+            if(search(i+dx[k],j+dy[k],board,word,currIndex+1)){
+                return true;
+            }
+        }
+        board[i][j]=temp;
         return false;
-
-    }
+    } 
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        for (int x=0;x<m;x++){
-            for (int y=0;y<n;y++){
-                if(isFound(board,word.c_str(),x,y)){
+        row = board.size();
+        col = board[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(search(i,j,board,word,0)){
                     return true;
                 }
             }
-        }
+          }
         return false;
-        
     }
 };
