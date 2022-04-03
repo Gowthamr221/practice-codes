ecff73a9c26b788baa25af01f9270395053ee1c5
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-03 17:29:40 +0530

    Time: 8 ms (38.40%), Space: 11.9 MB (93.15%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,24 @@
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int arrSize =nums.size();
+        int i,j;
+        for(i=arrSize-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+        }
+        if(i<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(j=arrSize-1;j>=0;j--){
+                if(nums[i]<nums[j])
+                break;
+            }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
     }
 };
