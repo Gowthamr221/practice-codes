04d6739b1a878d66d78d64ec624748785318f499
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-16 00:10:33 +0530

    Time: 15 ms (84.24%), Space: 23.9 MB (44.36%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -12,17 +12,20 @@
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-          if(!root) return nullptr;
-        
-        if(root->val < low) {
-            return trimBST(root->right, low, high);
-        } else if (root->val > high) {
-            return trimBST(root->left, low, high);
+         // base condition
+        if(root==nullptr){
+            return nullptr;
+        }
+        // if value is out of bounds
+        if(root->val > high){
+            return trimBST(root->left,low,high);
+        }
+        else if(root->val < low){
+            return trimBST(root->right,low,high);
         }
         
-        root->left = trimBST(root->left, low, high);
-        root->right = trimBST(root->right, low, high);
-        
+        root->left = trimBST(root->left,low,high);
+        root->right = trimBST(root->right,low,high);
         return root;
     }
 };
