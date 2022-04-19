27a4923d260cc85c3fa7487f90a5df25d0fd2c3a
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-19 18:22:16 +0530

    Time: 4 ms (42.48%), Space: 8.4 MB (42.96%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,23 +12,39 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        // vector<int>inorder;
+        // stack<TreeNode*>st;
+        // TreeNode* node = root;
+        // while(true){
+        //     if(node!=NULL){
+        //         st.push(node);
+        //         node = node->left;
+        //     }
+        //     else{
+        //         if(st.empty()==true)break;
+        //         node = st.top();
+        //         st.pop();
+        //         inorder.push_back(node->val);
+        //         node = node->right;
+        //     }
+        // }
+        // return inorder;
         vector<int>inorder;
         stack<TreeNode*>st;
-        TreeNode* node = root;
+        TreeNode*node = root;
         while(true){
-            if(node!=NULL){
+            if(node != NULL){
                 st.push(node);
                 node = node->left;
             }
             else{
-                if(st.empty()==true)break;
+                if(st.empty()==true) break;
                 node = st.top();
                 st.pop();
                 inorder.push_back(node->val);
-                node = node->right;
+                node=node->right;
             }
         }
         return inorder;
-        
     }
 };
