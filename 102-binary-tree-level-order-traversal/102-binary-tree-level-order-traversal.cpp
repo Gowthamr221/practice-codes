7563c5a87b62cf4f06934dea1bb633ae34777cfe
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-19 17:52:35 +0530

    Time: 11 ms (17.69%), Space: 12.7 MB (32.62%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -12,11 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
+        // vector<vector<int>> ans;
+        // if(root==NULL){
+        //     return ans;
+        // }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     vector<int>level;
+        //     int size = q.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
+        //         if(node->left!=NULL){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right!=NULL){
+        //             q.push(node->right);
+        //         }
+        //         level.push_back(node->val);
+        //     }
+        //     ans.push_back(level);
+        // }
+        // return ans;
+        vector<vector<int>>ans;
         if(root==NULL){
             return ans;
         }
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
             vector<int>level;
@@ -36,4 +59,5 @@ public:
         }
         return ans;
     }
+    
 };
