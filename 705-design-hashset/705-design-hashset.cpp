9e698db6bd29374e24ad859a8caaf91bdfbb8526
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-21 21:50:41 +0530

    Time: 141 ms (53.05%), Space: 41 MB (77.89%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,20 +1,39 @@
 class MyHashSet {
-    vector<int>arr;
+    vector<list<int>>m;
+    int siz;
 public:
     MyHashSet() {
-        arr.resize(1e6+1,0);
+        siz =100;
+        m.resize(siz);
     }
-    
+    int hash(int key){
+        return key%siz;
+    }
+    list<int> :: iterator search(int key){
+        int i = hash(key);
+        return find(m[i].begin(),m[i].end(),key); 
+    }
     void add(int key) {
-        arr[key]=1;
+        if(contains(key)){
+            return;
+        }
+        int i = hash(key);
+        m[i].push_back(key);
     }
     
     void remove(int key) {
-       arr[key]=0; 
+       if(!contains(key))return;
+       int i = hash(key);
+       m[i].erase(search(key));
+       
     }
     
     bool contains(int key) {
-        return arr[key];
+        int i = hash(key);
+        if(search(key)!=m[i].end()){
+            return true ;
+        }
+        return false;
     }
 };
 
