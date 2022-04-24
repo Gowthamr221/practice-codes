c422641c39f4bdff2aa2e8d4811db1fdea44d174
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-04-24 17:43:13 +0530

    Time: 192 ms (61.74%), Space: 57.5 MB (88.94%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,31 +1,39 @@
+
 class UndergroundSystem {
-    map <int,pair<string,int>>check;
-    map<pair<string,string>,vector<int>> storage;
+    map<int,pair<string,int>> checkInHistory;
+    map<pair<string,string>,pair<int,int>> storage;
 public:
     UndergroundSystem() {
         
     }
     
     void checkIn(int id, string stationName, int t) {
-        check[id] = {stationName,t};
+        checkInHistory[id]={stationName,t};
     }
     
     void checkOut(int id, string stationName, int t) {
-         pair<string,int> temp = check[id];
-        storage[{temp.first,stationName}].push_back(t-temp.second);
+        pair<string,int> temp = checkInHistory[id];
+        if(storage.find({temp.first,stationName})==storage.end()){
+            storage[{temp.first,stationName}]= {t-temp.second,1};
+         
+        }
+        else{
+             storage[{temp.first,stationName}].first+=t-temp.second;
+            storage[{temp.first,stationName}].second+=1;
+
+        }
+         
     }
     
     double getAverageTime(string startStation, string endStation) {
-        
         double avg = 0;
         if(storage.find({startStation,endStation})==storage.end()){
             return avg;
         }
-        vector<int>temp = storage[{startStation,endStation}];
-        for(auto it : temp){
-            avg +=it;
-        }
-        return avg/temp.size();
+        int totalHours = storage[{startStation,endStation}].first;
+        int totalCount = storage[{startStation,endStation}].second;
+        avg = (double)totalHours/(double)totalCount;
+        return avg;
     }
 };
 
