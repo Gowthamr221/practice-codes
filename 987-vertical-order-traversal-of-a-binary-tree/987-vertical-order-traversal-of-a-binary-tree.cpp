7f14454db63c8cb67342daca594ed7732bc72024
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-05-14 11:19:28 +0530

    Time: 3 ms (91.82%), Space: 13.5 MB (46.11%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,32 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> todo;
-        todo.push({root,{0,0}});
-        while(!todo.empty()){
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* Node = p.first;
-            int x = p.second.first,y=p.second.second;
-            nodes[x][y].insert(Node->val);
-            if(Node->left){
-                todo.push({Node->left,{x-1,y+1}});
+       map<int,map<int,multiset<int>>> nodes;
+       queue <pair<TreeNode*,pair<int,int>>> storage;
+       storage.push({root,{0,0}});
+       while(!storage.empty()){
+           pair<TreeNode*,pair<int,int>> p = storage.front();
+           storage.pop();
+           int x = p.second.first;
+           int y = p.second.second;
+           auto node = p.first;
+           nodes[x][y].insert(node->val);
+           if(node->left){
+               storage.push({node->left,{x-1,y+1}});
+           }
+           if(node->right){
+               storage.push({node->right,{x+1,y+1}});
+           }
+       } 
+        vector<vector<int>>ans;
+        for(auto p : nodes){
+            vector<int>col;
+            for(auto q : p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
             }
-            if(Node->right){
-                todo.push({Node->right,{x+1,y+1}});
-            }
+            ans.push_back(col);
         }
-            vector<vector<int>>ans;
-            for(auto p:nodes){
-                vector<int>col;
-                for(auto q:p.second){
-                    col.insert(col.end(),q.second.begin(),q.second.end());
-                }
-                ans.push_back(col);
-            }
-            return ans;
-      
+        
+       return ans;
         
     }
 };
