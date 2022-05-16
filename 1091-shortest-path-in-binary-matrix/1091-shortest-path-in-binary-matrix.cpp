46919d910efa5c9da435455f7732885845de7f1f
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-05-16 10:21:57 +0530

    Time: 70 ms (79.20%), Space: 20.2 MB (56.26%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if(grid[0][0]==1){
+            return -1;
+        }
+        queue<pair<int,pair<int,int>>> storage;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<pair<int,int>>dir = {{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+        storage.push({0,{0,0}});
+        grid[0][0]=1;
+        while(!storage.empty()){
+            int size = storage.size();
+            while(size--){
+                auto node = storage.front();
+                storage.pop();
+                auto point = node.second;
+                if(point.first==m-1 && point.second==n-1 ){
+                    return node.first+1;
+                }
+                for(auto it : dir){
+                    int r = point.first + it.first;
+                    int c = point.second + it.second;
+                    if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0){
+                        storage.push({node.first+1,{r,c}});
+                        
+                        grid[r][c]=1;
+                    }
+                }
+            
+            }
+            
+        }
+        return -1;
+    }
+};
