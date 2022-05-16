0b6eb6fe050dc76d7c08b107b72cea742b178d9f
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-05-16 13:57:25 +0530

    Time: 79 ms (68.60%), Space: 20 MB (60.85%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,37 +1,70 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+//         if(grid[0][0]==1){
+//             return -1;
+//         }
+//         queue<pair<int,pair<int,int>>> storage;
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<pair<int,int>>dir = {{1,1},{1,-1},{-1,1},{-1,-1},{0,1},{1,0},{0,-1},{-1,0}};
+//         storage.push({0,{0,0}});
+//         grid[0][0]=1;
+//         while(!storage.empty()){
+//             int size = storage.size();
+//             while(size--){
+//                 auto node = storage.front();
+//                 storage.pop();
+//                 auto point = node.second;
+//                 if(point.first==m-1 && point.second==n-1 ){
+//                     return node.first+1;
+//                 }
+//                 for(auto it : dir){
+//                     int r = point.first + it.first;
+//                     int c = point.second + it.second;
+//                     if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0){
+//                         storage.push({node.first+1,{r,c}});
+                        
+//                         grid[r][c]=1;
+//                     }
+//                 }
+            
+//             }
+            
+//         }
+//         return -1;
         if(grid[0][0]==1){
             return -1;
         }
-        queue<pair<int,pair<int,int>>> storage;
+        
+        queue<pair<pair<int,int>,int>> todo;
+        grid[0][0]=1;
+        todo.push({{0,0},0});
         int m = grid.size();
         int n = grid[0].size();
-        vector<pair<int,int>>dir = {{1,1},{1,-1},{-1,1},{-1,-1},{0,1},{1,0},{0,-1},{-1,0}};
-        storage.push({0,{0,0}});
-        grid[0][0]=1;
-        while(!storage.empty()){
-            int size = storage.size();
+        vector<pair<int,int>>dir = {{1,1},{0,1},{1,0},{1,-1},{-1,1},{0,-1},{-1,0},{-1,-1}};
+        while(!todo.empty()){
+            int size = todo.size();
             while(size--){
-                auto node = storage.front();
-                storage.pop();
-                auto point = node.second;
-                if(point.first==m-1 && point.second==n-1 ){
-                    return node.first+1;
+            auto currNode = todo.front();
+            todo.pop();
+            auto points = currNode.first;
+            auto value = currNode.second;
+            if(points.first == m-1 && points.second == n-1){
+                return value+1;
+            }
+            for(auto it : dir){
+                int r = points.first + it.first;
+                int c = points.second + it.second;
+                if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0){
+                    grid[r][c]=1;
+                    todo.push({{r,c},value+1});
                 }
-                for(auto it : dir){
-                    int r = point.first + it.first;
-                    int c = point.second + it.second;
-                    if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0){
-                        storage.push({node.first+1,{r,c}});
-                        
-                        grid[r][c]=1;
-                    }
-                }
-            
+            }    
             }
             
         }
         return -1;
+        
     }
 };
