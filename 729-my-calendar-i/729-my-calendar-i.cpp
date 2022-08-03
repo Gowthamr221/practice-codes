aa4c1979778f347106af855ed3b7fd3374f90a9c
KODURU SIVA GOWTHAM REDDY <33353028+Gowthamr221@users.noreply.github.com>
2022-08-04 00:33:26 +0530

    Time: 123 ms (78.62%), Space: 37.6 MB (94.67%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,10 +1,13 @@
 class MyCalendar {
-    vector<pair<int, int>> books;
+    vector<pair<int,int>>arr;
 public:    
     bool book(int start, int end) {
-        for (pair<int, int> p : books)
-            if (max(p.first, start) < min(end, p.second)) return false;
-        books.push_back({start, end});
+       for(auto it : arr){
+          if(max(it.first, start) < min(end, it.second)){
+              return false;
+          }
+       }
+        arr.push_back({start,end});
         return true;
     }
 };
